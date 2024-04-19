321240ee1132820252323093b160c383557e74cc
Hugo Miguel Trindade Parada <htrindad@student.42lisboa.com>
2024-04-19 14:12:55 +0100

    first test done

--- ft_split.c
+++ ft_split.c
@@ -6,53 +6,77 @@
 /*   By: htrindad <htrindad@student.42lisboa.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/18 19:49:30 by htrindad          #+#    #+#             */
-/*   Updated: 2024/04/18 20:47:07 by htrindad         ###   ########.fr       */
+/*   Updated: 2024/04/19 14:12:02 by htrindad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static void	ft_fnn(char **ss)
+static void	ft_free(char **ptr, size_t count)
 {
-	free(ss);
-	return (NULL);
+	while (count)
+		free(ptr[count--]);
+	free(ptr);
 }
 
-static char	**count_words(char const *s, char c)
+static size_t	ft_countc(char const *s, char c)
 {
-	int		x;
-	size_t	count;
-	char	**ss;
-	size_t	i;
+	size_t		count;
+	u_int8_t	once;
 
-	x = 1;
 	count = 0;
+	once = 0;
 	while (*s)
 	{
-		if (*s == c && !x)
-			x = 1;
-		else if (x)
+		if (*s == c && !once)
 		{
 			count++;
-			x = 0;
+			once = 1;
 		}
+		if (*s != c && once)
+			once = 0;
 		s++;
 	}
-	ss = malloc(sizeof(char *) * count + 1);
-	if (!ss)
-		return (NULL);
+	return (count);
+}
+
+static size_t	ft_fp(char const *s, char c, size_t count)
+{
+	size_t	i;
+
 	i = 0;
-	/*while (i < count)
+	while (count)
 	{
-		ss[i] = malloc(sizeof(char));
+		if (s[i] == c)
+			count++;
 		i++;
-	}*/
+	}
+	return (i);
+}
+
+static char	**ft_create_arr(char const *s, char c, char **ss, size_t con)
+{
+	size_t	i;
+
+	i = 0;
+	while (i < con)
+		ss[i++] = ft_substr(s, ft_fp(s, c, con - 1), ft_fp(s, c, con));
+	return (ss);
 }
 
 char	**ft_split(char const *s, char c)
 {
+	size_t	count;
 	char	**ss;
 
-	if (s == NULL)
+	if (!s)
 		return (NULL);
+	count = ft_countc(s, c);
+	ss = malloc(sizeof(char *) * (count + 1));
+	if (ss == NULL)
+	{
+		ft_free(ss, count);
+		return (NULL);
+	}
+	return (ft_create_arr(s, c, ss, count));
 }
