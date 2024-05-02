8b818f97832de43b0c11ed69d197919a12936e3e
Hugo Miguel Trindade Parada <htrindad@student.42lisboa.com>
2024-05-02 14:04:55 +0100

    done, I'll start the story from here the next time...

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: htrindad <htrindad@student.42lisboa.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/25 17:29:56 by htrindad          #+#    #+#             */
-/*   Updated: 2024/04/30 19:15:19 by htrindad         ###   ########.fr       */
+/*   Updated: 2024/05/02 14:00:50 by htrindad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -64,7 +64,7 @@ void		ft_lstadd_front(t_list **list, t_list *new);
 int			ft_lstsize(t_list *new);
 t_list		*ft_lstlast(t_list *lst);
 void		ft_lstadd_back(t_list **lst, t_list *new);
-void	ft_lstdelone(t_list *lst, void (*del)(void *));
+void		ft_lstdelone(t_list *lst, void (*del)(void *));
 void		ft_lstclear(t_list **lst, void (*del)(void *));
 void		ft_lstiter(t_list *lst, void (*f)(void *));
 t_list		*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *));
